ad4763362d147bad6009c0d939d3768052c36155
Camilo-ASC <Camiloasc30@gmail.com>
2025-04-17 22:09:11 -0500

    Fixes 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,29 +3,25 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'auth/login',
     pathMatch: 'full'
   },
   {
-    path: 'login',
-    loadChildren: () => import('./auth/pages/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'register',
-    loadChildren: () => import('./auth/pages/register/register.module').then( m => m.RegisterPageModule)
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: 'add-contact',
-    loadChildren: () => import('./add-contact/add-contact.module').then( m => m.AddContactPageModule)
+    loadChildren: () => import('./add-contact/add-contact.module').then(m => m.AddContactPageModule)
   },
   {
     path: 'call-handler',
-    loadChildren: () => import('./call-handler/call-handler.module').then( m => m.CallHandlerPageModule)
+    loadChildren: () => import('./call-handler/call-handler.module').then(m => m.CallHandlerPageModule)
   },
 ];
 
